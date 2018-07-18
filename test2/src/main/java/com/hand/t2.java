b4a8ad10fcf99229b7c985c792e22bfd26f43743
lisiyu <18392955246@163.com>
2018-07-18 14:52:55 +0800

    修改文件

--- test2/src/main/java/com/hand/t2.java
+++ test2/src/main/java/com/hand/t2.java
@@ -2,6 +2,6 @@ package com.hand;
 
 public class t2 {
     public static void main(String[] args) {
-        System.out.printf("I am t2....");
+        System.out.printf("I am t2.......");
     }
 }
